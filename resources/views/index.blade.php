fe3a33bc46a3fe984a9a0230f77ddd7400a3a91f
jijicries <akicarinan0216@gmail.com>
2025-01-13 12:47:02 +0800

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+<div class="bg-white">
+  <header class="absolute inset-x-0 top-0 z-50">
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <div class="flex items-center justify-center mx-auto max-w-screen-xl p-4">
+          <a href="" class="flex items-center space-x-4 rtl:space-x-reverse">
+              <img src="img/rahs_logo-removebg.png" class="h-24" alt="RAHS Logo" />
+              <div class="flex flex-col">
+                <span class="text-6xl font-bold text-maroon">RAHS</span> <!-- Updated to maroon -->
+                <span class="text-lg font-semibold text-maroon">
+                    Red Arrow High School
+                </span> <!-- Updated to maroon -->
+                <span class="text-sm  text-gray-500 dark:text-white hover:underline">
+                    Magsaysay St. San Nicolas, Pangasinan
+                </span> <!-- Updated to maroon -->
+            </div>
+          </a>
+      </div>
+    </nav>
+    <!-- <nav class="bg-gray-50 dark:bg-gray-700">
+        <div class="max-w-screen-xl px-4 py-3 mx-auto">
+            <div class="flex items-center">
+                <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav> -->
+
+    <nav class="bg-whitesmoke">
+      <div class="max-w-screen-xl px-4 py-3 mx-auto">
+          <div class="flex items-center">
+              <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-m">
+                  <li>
+                      <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                  </li>
+                  <li>
+                      <a href="#" class="text-gray-900 dark:text-white hover:underline">About Us</a>
+                  </li>
+                  <!-- <li>
+                      <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
+                  </li> -->
+                  <li>
+                      <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+    </nav>
+  </header>
+
+  <div class="bg-white">
+    <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+      <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
+    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div class="">
+      <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Empowering Minds, Shaping Futures – Discover Excellence at <span class="text-maroon">Red Arrow High School</span></h1>
+      <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+
+      </div>
+    </div>
+    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+      <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
+  </div>
+
+  
+
+  <footer class="bg-white ">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="" class="hover:underline">Red Arrow High School™</a>. All Rights Reserved.</span>
+      </div>
+  </footer>
+
+
+</div>
+
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        maroon: '#800000', // Define maroon color
+        whitesmoke: '#F5F5F5'
+      },
+    },
+  },
+  plugins: [],
+}
